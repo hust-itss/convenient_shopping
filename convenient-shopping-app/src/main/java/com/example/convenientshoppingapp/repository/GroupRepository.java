9404d0e1a57d4f7092b03df3c4e974c3726a6e69
Lê Thế Anh-Intern <theanh19082002@gmail.com>
2023-06-16 08:25:11 +0700

    add CRUD dish entity and dish controller

--- convenient-shopping-app/src/main/java/com/example/convenientshoppingapp/repository/GroupRepository.java
+++ convenient-shopping-app/src/main/java/com/example/convenientshoppingapp/repository/GroupRepository.java
@@ -4,6 +4,14 @@ import com.example.convenientshoppingapp.entity.Group;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface GroupRepository extends JpaRepository<Group, Long> {
+
+    Optional<Group> findByName(String name);
+    Optional<Group> findById(Long id);
+
+    Optional<Group> findByGroupLeader(Long id);
+    Optional<Group> deleteGroupById(Long id);
 }
--- convenient-shopping-app/src/main/java/com/example/convenientshoppingapp/service/impl/GroupService.java
+++ convenient-shopping-app/src/main/java/com/example/convenientshoppingapp/service/impl/GroupService.java
@@ -1,9 +1,16 @@
 package com.example.convenientshoppingapp.service.impl;
 
+import com.example.convenientshoppingapp.repository.GroupRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class GroupService {
+
+    private final GroupRepository groupRepository;
+
+    public
 }
