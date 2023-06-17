fe27bb133136d68a73ce0692740a334ab757a1e4
AnhTheLe <84082409+AnhTheLe@users.noreply.github.com>
2023-06-17 21:30:07 +0700

    Update GroupService.java

--- convenient-shopping-app/src/main/java/com/example/convenientshoppingapp/service/impl/GroupService.java
+++ convenient-shopping-app/src/main/java/com/example/convenientshoppingapp/service/impl/GroupService.java
@@ -17,57 +17,5 @@ import java.util.Optional;
 @RequiredArgsConstructor
 @Slf4j
 public class GroupService {
-    private final UserRepository userRepository;
-
-    private final GroupRepository groupRepository;
-
-    public void deleteGroupById(Long id) {
-        groupRepository.deleteGroupById(id);
-    }
-
-
-    @Modifying
-    public Group save(Group group) {
-        if(groupRepository.findByName(group.getName()).isPresent()){
-            throw new RuntimeException("Group name already exists");
-        }
-        log.info("Group: {}", group);
-        return groupRepository.save(group);
-    }
-
-    @Modifying
-    public Group update(Group group){
-        Group oldGroup = groupRepository.findById(group.getId())
-                .orElseThrow(() -> new RuntimeException("Group not found with id: " + group.getId()));
-        oldGroup.setName(group.getName());
-        oldGroup.setGroupLeader(group.getGroupLeader());
-        log.info("Group: {}", group);
-        return groupRepository.save(oldGroup);
-    }
-
-    @Modifying
-    public void addMember(String nameNember, String nameGroup) {
-        Group group = groupRepository.findByName(nameGroup)
-                .orElseThrow(() -> new RuntimeException("Group not found with name: " + nameGroup));
-        Optional<Users> user = userRepository.findByUsername(nameNember);
-        if (group != null && user.isPresent()) {
-            if (group.getUsers() == null) {
-                group.setUsers(new HashSet<>()); // Khởi tạo user là một HashSet mới nếu roles là null
-            }
-            group.getUsers().add(user.get());
-        }
-    }
-
-    @Modifying
-    public void deleteMember(String nameMember, String nameGroup) {
-        Group group = groupRepository.findByName(nameGroup)
-                .orElseThrow(() -> new RuntimeException("Group not found with name: " + nameGroup));
-        Optional<Users> user = userRepository.findByUsername(nameMember);
-        if (group != null && user.isPresent()) {
-            if (group.getUsers() == null) {
-                group.setUsers(new HashSet<>()); // Khởi tạo user là một HashSet mới nếu roles là null
-            }
-            group.getUsers().remove(user.get());
-        }
-    }
+   
 }
