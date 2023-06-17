4c8a1688c7b236819163d1d865cf16d0190887c1
AnhTheLe <84082409+AnhTheLe@users.noreply.github.com>
2023-06-17 21:27:18 +0700

    Update GroupController.java

--- convenient-shopping-app/src/main/java/com/example/convenientshoppingapp/controller/GroupController.java
+++ convenient-shopping-app/src/main/java/com/example/convenientshoppingapp/controller/GroupController.java
@@ -14,24 +14,5 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class GroupController {
 
-    private GroupService groupService;
 
-    @PostMapping("")
-    public ResponseEntity<ResponseObject> insert(@RequestBody @Valid Group group) {
-        groupService.save(group);
-        return ResponseEntity.status(HttpStatus.CREATED).body(new ResponseObject("success", "Insert dữ liệu thành công", group));
-    }
-
-    @PutMapping("/{id}")
-    public ResponseEntity<ResponseObject> update(@PathVariable Long id, @RequestBody @Valid Group group) {
-        group.setId(id);
-        groupService.update(group);
-        return ResponseEntity.status(HttpStatus.OK).body(new ResponseObject("success", "Cập nhật dữ liệu thành công", group));
-    }
-
-    @DeleteMapping("/removeMember")
-    public ResponseEntity<ResponseObject> removeMember(@PathVariable String nameMember, @RequestBody @Valid String nameGroup) {
-        groupService.deleteMember(nameMember, nameGroup);
-        return ResponseEntity.status(HttpStatus.OK).body(new ResponseObject("success", "Cập nhật dữ liệu thành công", ""));
-    }
 }
