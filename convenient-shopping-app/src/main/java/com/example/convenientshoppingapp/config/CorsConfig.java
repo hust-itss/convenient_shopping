c67d0b18dcd9e28745152bb7f2446c67f5e2c8e5
Lê Thế Anh-Intern <theanh19082002@gmail.com>
2023-06-29 09:47:30 +0700

    reconfig cross

--- convenient-shopping-app/src/main/java/com/example/convenientshoppingapp/config/CorsConfig.java
+++ convenient-shopping-app/src/main/java/com/example/convenientshoppingapp/config/CorsConfig.java
@@ -3,12 +3,14 @@ package com.example.convenientshoppingapp.config;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 
 import java.io.IOException;
 
 @Configuration
+@Slf4j
 public class CorsConfig implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
@@ -16,13 +18,14 @@ public class CorsConfig implements Filter {
 
         final HttpServletResponse httpServletResponse = (HttpServletResponse) response;
 
-        httpServletResponse.setHeader("Access-Control-Allow-Origin", "*");
+        httpServletResponse.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
         httpServletResponse.setHeader("Access-Control-Allow-Methods", "*");
         httpServletResponse.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type");
         httpServletResponse.setHeader("Access-Control-Max-Age", "3600");
         if (HttpMethod.OPTIONS.name().equalsIgnoreCase(((HttpServletRequest) request).getMethod())) {
             httpServletResponse.setStatus(HttpServletResponse.SC_OK);
         }
+        log.info("Cors filter");
         chain.doFilter(request, response);
     }
 }
--- convenient-shopping-app/src/main/java/com/example/convenientshoppingapp/controller/DishController.java
+++ convenient-shopping-app/src/main/java/com/example/convenientshoppingapp/controller/DishController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/dish")
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RequiredArgsConstructor
 public class DishController {
 
