e12b662783b14354c8f17a3dc23ab8b96bf0cd00
The Anh <anh.pt20491@sis.hust.edu.vn>
2023-07-05 16:38:46 +0700

    unit test

--- convenient-shopping-app/src/test/java/com/example/convenientshoppingapp/ConvenientShoppingAppApplicationTests.java
+++ convenient-shopping-app/src/test/java/com/example/convenientshoppingapp/ConvenientShoppingAppApplicationTests.java
@@ -1,5 +1,9 @@
 package com.example.convenientshoppingapp;
 
+import com.example.convenientshoppingapp.dto.auth.AuthenticationRequest;
+import com.example.convenientshoppingapp.entity.Dish;
+import com.example.convenientshoppingapp.service.impl.DishService;
+import com.example.convenientshoppingapp.service.impl.auth.AuthenticationService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -9,5 +13,7 @@ class ConvenientShoppingAppApplicationTests {
     @Test
     void contextLoads() {
     }
+
+
 
 }
